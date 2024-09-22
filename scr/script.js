9b8bfd5d8fe108bb8ea6e68da6642c8ea052c2fb
annaaani <ani.annabrita@gmail.com>
2024-09-22 08:48:20 +0000

    scientific calculator

---
+++ scr/script.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", function (){
+    const display = document.getElementById('calc-display');
+    const buttons = document.getElementsByClassName('btn');
+
+    let currentValue="";
+
+    function evaluateResult(){
+        const convertedValue = currentValue
+        .replace("×", "*")
+        .replace("÷", "/")
+        .replace('%','*0.01')
+        .replace('sin', 'Math.sin')
+        .replace('cos','Math.cos')
+        .replace('ln','Math.log')
+        .replace('π','Math.PI')
+        .replace('log','Math.log10')
+        .replace('e','Math.e')
+        .replace('tan','Math.tan')
+        .replace('√','Math.sqrt');
+        const result = eval(convertedValue);
+        currentValue = result.toString();
+        display.value = currentValue
+    }
+
+    for (let i=0; i< buttons.length; i++){
+        const button = buttons[i];
+        button.addEventListener('click', function(){
+            const value = button.innerText;
+
+            try{
+                if(value == "AC"){
+                    currentValue = "";
+                    display.value = currentValue; 
+                }else if(value == "="){
+                    evaluateResult();
+                }
+                else{
+                    currentValue += value;
+                    display.value = currentValue
+                }
+            }
+            catch (error){
+                console.error(error);
+                currentValue = "ERROR";
+                display.value = currentValue;
+            }
+        })
+    }
+
+});
